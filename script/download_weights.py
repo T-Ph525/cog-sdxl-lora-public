0276a7ac9ee5fae591065f0436648531b48db539
Evanʎx <95352755+T-Ph525@users.noreply.github.com>
2025-02-19 05:27:01 +1100

    Update download_weights.py

--- script/download_weights.py
+++ script/download_weights.py
@@ -4,9 +4,6 @@
 
 import torch
 from diffusers import AutoencoderKL, StableDiffusionXLPipeline
-from diffusers.pipelines.stable_diffusion.safety_checker import (
-    StableDiffusionSafetyChecker,
-)
 
 # pipe = StableDiffusionXLPipeline.from_pretrained(
 #     "stabilityai/stable-diffusion-xl-base-1.0",
@@ -16,35 +13,3 @@ from diffusers.pipelines.stable_diffusion.safety_checker import (
 # )
 
 # pipe.save_pretrained("./cache", safe_serialization=True)
-
-better_vae = AutoencoderKL.from_pretrained(
-    "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
-)
-
-pipe = StableDiffusionXLPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    vae=better_vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-
-pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
-
-pipe = StableDiffusionXLPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-
-# TODO - we don't need to save all of this and in fact should save just the unet, tokenizer, and config.
-pipe.save_pretrained("./refiner-cache", safe_serialization=True)
-
-
-safety = StableDiffusionSafetyChecker.from_pretrained(
-    "CompVis/stable-diffusion-safety-checker",
-    torch_dtype=torch.float16,
-)
-
-safety.save_pretrained("./safety-cache")
